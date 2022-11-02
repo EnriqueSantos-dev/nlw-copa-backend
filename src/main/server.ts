11e2fabdbc87d5c46d4e6089412a1a2a56a883f0
Enrique Santos <santosnrique2121@gmail.com>
2022-11-02 14:30:07 -0300

    chore: add fastifyCors library

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,82 +1,106 @@
 import 'dotenv/config'
 import Fastify, { FastifyReply, FastifyRequest } from 'fastify'
+import cors from '@fastify/cors'
 import { z } from 'zod'
 import Prisma from './factories/prisma-client'
 import ShortUniqueId from 'short-unique-id'
 
 const prisma = Prisma.getInstance()
 
-const bootsStrap = (): void => {
+const bootsStrap = async (): Promise<void> => {
   const fastify = Fastify({
     logger: true
   })
 
-  fastify.get('/users/count', async (request: FastifyRequest, reply: FastifyReply) => {
-    try {
-      const lengthUsers = await prisma.user.count()
+  await fastify.register(cors)
 
-      return await reply.code(200).send({ count: lengthUsers })
-    } catch (error) {
-      return await reply.code(500).send('Internal Server Error')
-    }
-  })
+  fastify.get(
+    '/users/count',
+    async (request: FastifyRequest, reply: FastifyReply) => {
+      try {
+        const lengthUsers = await prisma.user.count()
 
-  fastify.get('/guesses/count', async (request: FastifyRequest, reply: FastifyReply) => {
-    try {
-      const lengthGuesses = await prisma.guess.count()
-
-      return await reply.code(200).send({ count: lengthGuesses })
-    } catch (error) {
-      return await reply.code(500).send('Internal Server Error')
-    }
-  })
-
-  fastify.get('/pools/count', async (request: FastifyRequest, reply: FastifyReply) => {
-    try {
-      const pools = await prisma.pool.count()
-
-      return await reply.code(200).send({ count: pools })
-    } catch (error) {
-      return await reply.code(500).send('Internal Server Error')
-    }
-  })
-
-  fastify.post('/pools', async (request: FastifyRequest, reply: FastifyReply) => {
-    const schemaCreatePool = z.object({
-      title: z.string({ required_error: 'O nome é obrigatório para criar o bolão' }).min(5, {
-        message: 'O nome do bolão precisa ter pelo menos 5 letras'
-      })
-    })
-
-    try {
-      const requestValidated = schemaCreatePool.safeParse(request.body)
-
-      if (!requestValidated.success) {
-        return await reply.code(400).send(requestValidated.error.format())
+        return await reply.code(200).send({ count: lengthUsers })
+      } catch (error) {
+        return await reply.code(500).send('Internal Server Error')
       }
-
-      const generateCode = new ShortUniqueId({ length: 6 })
-
-      const code = await prisma.pool.create({
-        select: {
-          code: true
-        },
-        data: {
-          title: requestValidated.data.title,
-          code: String(generateCode()).toUpperCase()
-        }
-      })
-      return await reply.code(201).send(code)
-    } catch (error) {
-      return await reply.code(500).send('Internal server error')
     }
-  })
+  )
 
-  fastify.listen({
-    port: Number(process.env.PORT) ?? 3333
-  }, () => {
-    console.log(`http server listening on port  ${process.env.PORT ?? '3333'}`)
-  })
+  fastify.get(
+    '/guesses/count',
+    async (request: FastifyRequest, reply: FastifyReply) => {
+      try {
+        const lengthGuesses = await prisma.guess.count()
+
+        return await reply.code(200).send({ count: lengthGuesses })
+      } catch (error) {
+        return await reply.code(500).send('Internal Server Error')
+      }
+    }
+  )
+
+  fastify.get(
+    '/pools/count',
+    async (request: FastifyRequest, reply: FastifyReply) => {
+      try {
+        const pools = await prisma.pool.count()
+
+        return await reply.code(200).send({ count: pools })
+      } catch (error) {
+        return await reply.code(500).send('Internal Server Error')
+      }
+    }
+  )
+
+  fastify.post(
+    '/pools',
+    async (request: FastifyRequest, reply: FastifyReply) => {
+      const schemaCreatePool = z.object({
+        title: z
+          .string({ required_error: 'O nome é obrigatório para criar o bolão' })
+          .min(5, {
+            message: 'O nome do bolão precisa ter pelo menos 5 letras'
+          })
+      })
+
+      try {
+        const requestValidated = schemaCreatePool.safeParse(request.body)
+
+        if (!requestValidated.success) {
+          return await reply.code(400).send(requestValidated.error.format())
+        }
+
+        const generateCode = new ShortUniqueId({ length: 6 })
+
+        const code = await prisma.pool.create({
+          select: {
+            code: true
+          },
+          data: {
+            title: requestValidated.data.title,
+            code: String(generateCode()).toUpperCase()
+          }
+        })
+        return await reply.code(201).send(code)
+      } catch (error) {
+        return await reply.code(500).send('Internal server error')
+      }
+    }
+  )
+
+  fastify.listen(
+    {
+      port: Number(process.env.PORT) ?? 3333
+    },
+    () => {
+      console.log(
+        `http server listening on port  ${process.env.PORT ?? '3333'}`
+      )
+    }
+  )
 }
 
-bootsStrap()
+(async () => {
+  await bootsStrap()
+})().catch((error) => console.error(error))
